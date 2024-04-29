61a882b9756ff5edaf76591e193a879d2e03ef08
futuroDevLeo <leonunes07@outlook.com>
2024-04-28 22:15:27 -0300

    feat(ALURA-ORACLE-ONE): first project with java

---
+++ Alura-Oracle-ONE/src/Perfil.java
@@ -0,0 +1,15 @@
+// # 1
+// Crie um projeto Java no IntelliJ que deve conter uma classe chamada Perfil,
+// com o método main, que ao executar deverá imprimir um cumprimento personalizado
+// no console. Por exemplo, "Olá, [Seu Nome]!".
+
+// # 2
+// Modifique o programa para imprimir uma segunda mensagem de cumprimento
+// personalizada. Por exemplo, "Tudo bem?".
+
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá, Léo!");
+        System.out.println("Tudo bem?");
+    }
+}
---
+++ Alura-Oracle-ONE/src/Subtracao.java
@@ -0,0 +1,9 @@
+// # 4
+// Crie uma classe Subtração para imprimir o resultado de 10 - 5 no console.
+
+public class Subtracao {
+
+    public static void main(String[] args) {
+        System.out.println(10 - 5);
+    }
+}
